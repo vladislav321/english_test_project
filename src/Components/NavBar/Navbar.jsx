451cc69db1811abae32f162cb07772ddaea25147
Vlad Godovany <vgodov02@gmail.com>
2020-02-24 21:08:57 +0200

    refactoring

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () =>{
     return(
         <nav className={s.nav}>
         <div className={s.item}>
-          <NavLink to="/listWords" activeClassName={s.active}> Profile </NavLink>
+          <NavLink to="/listWords" activeClassName={s.active}> list words </NavLink>
         </div>
         <div className={s.item}>
-          <NavLink to="/chooseLessons" activeClassName={s.active}> Messages </NavLink>
+          <NavLink to="/chooseLessons" activeClassName={s.active}> choose lessons </NavLink>
         </div>
     </nav>
     );
