91484c1cc087422b9e92495cb8aafaefcd4a2f97
Vlad Godovany <vgodov02@gmail.com>
2020-02-26 17:07:49 +0200

    set Edit mode for show translate

--- src/Components/ListWords/ListWords.jsx
+++ src/Components/ListWords/ListWords.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './ListWords.module.css';
 
 const ListWords = (props) => {
     let word = props.model.Ru;
 
- 
+    let [editMode, setEditMode] = useState(false);
+
+    const onEditMode = () => {
+        let mode = editMode = !editMode;
+        setEditMode(mode);
+    }
+    
     return(
         <div className={style.wordsContainer} >
             <div className={style.wordText}> 
                  {word} 
             </div>
-            <div className={style.wordTranslate}>
-                 Показать перевод 
+            <div className={style.wordTranslate} onDoubleClick={onEditMode}>
+                {!editMode ? "Показать перевод" : props.model.En}
             </div>
 
             <input 
